9b9579efa3d4fd27e6980c125f2d7ceb909595d0
hhyvs111 <445622919@qq.com>
2019-02-22 15:03:28 +0800

    delete

--- Base/noncopyable.h
+++ Base/noncopyable.h
@@ -5,14 +5,15 @@
 #ifndef WEBSERVER_NONCOPYABLE_H
 #define WEBSERVER_NONCOPYABLE_H
 #pragma once
-//不可复制的类，就是直接把拷贝函数声明为私有的不就等于禁用
+//不可复制的类，就是直接把拷贝函数声明为私有的就等于禁用,或者声明为delete
 class noncopyable
 {
 protected:
     noncopyable() {}
     ~noncopyable() {}
-private:
-    noncopyable(const noncopyable&);
-    const noncopyable& operator=(const noncopyable&);
+
+public:
+    noncopyable(const noncopyable&) = delete;
+    const noncopyable& operator=(const noncopyable&) = delete;
 };
 #endif //WEBSERVER_NONCOPYABLE_H
